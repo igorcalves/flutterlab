3814869070ea61a405e400b5c0c52d3557935281
IGOR_ALVES_DA_SILVA_PEREIRA <igor193@uni9.edu.br>
2024-04-01 09:05:38 +0000

    t

--- lib/test.dart
+++ lib/test.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 
 void main() async {
-  var url = Uri.parse('http://192.168.56.1:8080/users');
+  var url = Uri.parse(
+      'https://8080-igorcalves-condominusba-8hnd04t45m2.ws-us110.gitpod.io/users');
   var response = await http.get(url);
 
   if (response.statusCode == 200) {
